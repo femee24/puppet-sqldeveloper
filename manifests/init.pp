f24c81ae93f93fcd440f15596f0ee2faa1b00788
femee24 <fhem24_elle@yahoo.com>
2015-07-02 15:19:11 +0800

    added boot2docker

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,7 @@
 # This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+class boot2docker {
+package { 'boot2docker':
+provider => 'pkgdmg',
+source   => 'https://github.com/boot2docker/osx-installer/releases/download/v1.7.0/Boot2Docker-1.7.0.pkg'
+}
 }
